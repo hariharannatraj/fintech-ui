da7ae202e0d9ec77a51ed0e0fd44a2d1ff10e4de
Kartheeswaran M <kartheeswaran@myambergroup.com>
2021-09-11 15:44:51 +0530

    loan details page added

--- src/app/modules/components/loan-info/user-needs/user-needs.component.ts
+++ src/app/modules/components/loan-info/user-needs/user-needs.component.ts
@@ -12,18 +12,47 @@ import { PrimeNGConfig } from 'primeng/api';
 export class UserNeedsComponent implements OnInit {
   displayModal!: boolean;
   amt_need: number = 10000;
-  tenure!: number;
-  interest_rate!: number;
+  tenure: number = 12;
+  interest_rate: number = 5;
+  monthly_emi!: number;
   constructor(private primengConfig: PrimeNGConfig) { }
   
   ngOnInit(): void {
     this.primengConfig.ripple = true;
   }
 
-  
+  calculate_emi() {
+    this.monthly_emi = this.amt_need * this.interest_rate * (1 + this.interest_rate)^this.tenure /  ((1 + this.interest_rate)^this.tenure - 1);
+  }
 
   showModalDialog() {
     this.displayModal = true;
+  }
+
+  loan_options = [
+    { text: 'Holiday Loan', isClicked: false },
+    { text: 'Vacation Loan', isClicked: false },
+    { text: 'Home Repairs', isClicked: false },
+    { text: 'Lifestyle product purchases', isClicked: false },
+    { text: 'Big Purchase', isClicked: false },
+    { text: 'Wedding', isClicked: false },
+    { text: 'Business expansion', isClicked: false },
+    { text: 'Cash against existing owned property', isClicked: false },
+    { text: 'New Home purchase', isClicked: false },
+    { text: 'Auto / Motor purchase', isClicked: false },
+    { text: 'New Home purchase', isClicked: false },
+    { text: 'Special Occasion need', isClicked: false },
+    { text: 'Education Loan', isClicked: false },
+    { text: 'Dept Consolidation', isClicked: false },
+  ]
+
+
+  selectLoan(loan: any): void {
+    for(let loan of this.loan_options) {
+      loan.isClicked = false;
+    }
+
+    loan.isClicked = true;
   }
 
 }
